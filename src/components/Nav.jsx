453366dbdcb503b7cc57242aaddf804aaeb7474d
kfblack <kalinblack21@gmail.com>
2024-02-07 11:48:57 -0500

    Minor changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom'
 const Nav = () => {
     return (
         <div>
-            <NavLink to='/lesson/1'>Lesson 1</NavLink>
-            <NavLink to='/lesson/2'>Lesson 2</NavLink>
-            <NavLink to='/lesson/3'>Lesson 3</NavLink>
+            <NavLink to='/lessons/1'>Lesson 1</NavLink>
+            <NavLink to='/lessons/2'>Lesson 2</NavLink>
+            <NavLink to='/lessons/3'>Lesson 3</NavLink>
         </div>
     )
 }
